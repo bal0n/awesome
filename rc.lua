68344933114c9011d8839c84fea71aa9637ee364
Francisco Javier <32569754+bal0n@users.noreply.github.com>
2017-12-20 16:28:06 +0100

    Depuración código de rc.lua

--- rc.lua
+++ rc.lua
@@ -1,33 +1,24 @@
--- Standard awesome library
-local gears = require("gears")
-local awful = require("awful")
-require("awful.autofocus")
--- Widget and layout library
-local wibox = require("wibox")
--- Theme handling library
-local beautiful = require("beautiful")
--- Notification library
-local naughty = require("naughty")
-local menubar = require("menubar")
-local hotkeys_popup = require("awful.hotkeys_popup").widget
--- Enable hotkeys help widget for VIM and other apps
--- when client with a matching name is opened:
-require("awful.hotkeys_popup.keys")
+-- Librerías estándar
+local gears 		= 	require("gears")
+local awful 		= 	require("awful")
+				require("awful.autofocus")
+local wibox 		= 	require("wibox")
+local beautiful 	= 	require("beautiful")
+local naughty 		= 	require("naughty")
+local menubar 		= 	require("menubar")
+local hotkeys_popup 	= 	require("awful.hotkeys_popup").widget
+				require("awful.hotkeys_popup.keys")
 
--- {{{ Error handling
--- Check if awesome encountered an error during startup and fell back to
--- another config (This code will only ever execute for the fallback config)
+-- Errores
 if awesome.startup_errors then
     naughty.notify({ preset = naughty.config.presets.critical,
                      title = "Oops, there were errors during startup!",
                      text = awesome.startup_errors })
 end
 
--- Handle runtime errors after startup
 do
     local in_error = false
     awesome.connect_signal("debug::error", function (err)
-        -- Make sure we don't go into an endless error loop
         if in_error then return end
         in_error = true
 
@@ -37,28 +28,18 @@ do
         in_error = false
     end)
 end
--- }}}
 
--- AUTORUN
+-- Autorun (mediante script)
 awful.spawn.with_shell("~/.config/awesome/autorun.sh")
 
--- {{{ Variable definitions
--- Themes define colours, icons, font and wallpapers.
+-- Variables
 beautiful.init("~/.config/awesome/tm.lua")
-
--- This is used later as the default terminal and editor to run.
 terminal = "termite"
 editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
-
--- Default modkey.
--- Usually, Mod4 is the key with a logo between Control and Alt.
--- If you do not like this or do not have such a key,
--- I suggest you to remap Mod4 to another key using xmodmap or other tools.
--- However, you can use another modifier like Mod1, but it may interact with others.
 modkey = "Mod4"
 
--- Table of layouts to cover with awful.layout.inc, order matters.
+-- Layouts
 awful.layout.layouts = {
     awful.layout.suit.floating,
     awful.layout.suit.tile,
@@ -77,9 +58,9 @@ awful.layout.layouts = {
     -- awful.layout.suit.corner.sw,
     -- awful.layout.suit.corner.se,
 }
--- }}}
 
--- {{{ Helper functions
+
+-- Helper functions
 local function client_menu_toggle_fn()
     local instance = nil
 
@@ -92,10 +73,8 @@ local function client_menu_toggle_fn()
         end
     end
 end
--- }}}
 
--- {{{ Menu
--- Create a launcher widget and a main menu
+-- Menu
 myawesomemenu = {
    { "hotkeys", function() return false, hotkeys_popup.show_help end},
    { "manual", terminal .. " -e man awesome" },
@@ -113,14 +92,11 @@ mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon,
                                      menu = mymainmenu })
 
 -- Menubar configuration
-menubar.utils.terminal = terminal -- Set the terminal for applications that require it
--- }}}
-
--- Keyboard map indicator and switcher
+menubar.utils.terminal = terminal 
 mykeyboardlayout = awful.widget.keyboardlayout()
 
--- {{{ Wibar
--- Create a textclock widget
+-- Wibar
+---- Widget de reloj
 mytextclock = wibox.widget.textclock()
 
 -- Create a wibox for each screen and add it
