150b1ae7f27573be30ab9e1800e4dee1de908bb6
franciscodejavier <franciscojavierba@protonmail.com>
2018-03-04 00:12:13 +0100

    Cambiado rc.lua para restructuración

--- rcii.lua
+++ rcii.lua
@@ -1,32 +1,31 @@
--- Standard awesome library
-local gears = require("gears")
-local awful = require("awful")
-require("awful.autofocus")
--- Widget and layout library
-local wibox = require("wibox")
--- Theme handling library
-local beautiful = require("beautiful")
--- Notification library
-local naughty = require("naughty")
-local menubar = require("menubar")
-local hotkeys_popup = require("awful.hotkeys_popup").widget
-require("awful.hotkeys_popup.keys")
-local bateria = require("widgets.bateria")
+-- Librerías estándar
+local gears 		= 	require("gears")
+local awful 		= 	require("awful")
+				require("awful.autofocus")
+local wibox 		= 	require("wibox")
+local beautiful 	= 	require("beautiful")
+local naughty 		= 	require("naughty")
+local menubar 		= 	require("menubar")
+local hotkeys_popup 	= 	require("awful.hotkeys_popup").widget
+				require("awful.hotkeys_popup.keys")
 
--- {{{ Error handling
--- Check if awesome encountered an error during startup and fell back to
--- another config (This code will only ever execute for the fallback config)
+-- Librerías personales
+local bateria = require("widgets.bateria")
+local brillo = require("widgets.brillo")
+local cpu = require("widgets.cpu")
+local ram = require("widgets.ram")
+local volumen = require("widgets.volumen")
+
+-- Errores
 if awesome.startup_errors then
     naughty.notify({ preset = naughty.config.presets.critical,
                      title = "Oops, there were errors during startup!",
                      text = awesome.startup_errors })
 end
 
--- Handle runtime errors after startup
 do
     local in_error = false
     awesome.connect_signal("debug::error", function (err)
-        -- Make sure we don't go into an endless error loop
         if in_error then return end
         in_error = true
 
@@ -36,25 +35,18 @@ do
         in_error = false
     end)
 end
--- }}}
 
--- {{{ Variable definitions
--- Themes define colours, icons, font and wallpapers.
-beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+-- Autorun (mediante script)
+awful.spawn.with_shell("~/.config/awesome/scripts/autorun.sh")
 
--- This is used later as the default terminal and editor to run.
-terminal = "xterm"
+-- Variables
+beautiful.init("~/.config/awesome/tm.lua")
+terminal = "termite"
 editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
-
--- Default modkey.
--- Usually, Mod4 is the key with a logo between Control and Alt.
--- If you do not like this or do not have such a key,
--- I suggest you to remap Mod4 to another key using xmodmap or other tools.
--- However, you can use another modifier like Mod1, but it may interact with others.
 modkey = "Mod4"
 
--- Table of layouts to cover with awful.layout.inc, order matters.
+-- Layouts
 awful.layout.layouts = {
     awful.layout.suit.floating,
     awful.layout.suit.tile,
@@ -73,9 +65,9 @@ awful.layout.layouts = {
     -- awful.layout.suit.corner.sw,
     -- awful.layout.suit.corner.se,
 }
--- }}}
 
--- {{{ Helper functions
+
+-- Helper functions
 local function client_menu_toggle_fn()
     local instance = nil
 
@@ -88,10 +80,8 @@ local function client_menu_toggle_fn()
         end
     end
 end
--- }}}
 
--- {{{ Menu
--- Create a launcher widget and a main menu
+-- Menu
 myawesomemenu = {
    { "hotkeys", function() return false, hotkeys_popup.show_help end},
    { "manual", terminal .. " -e man awesome" },
@@ -109,14 +99,11 @@ mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon,
                                      menu = mymainmenu })
 
 -- Menubar configuration
-menubar.utils.terminal = terminal -- Set the terminal for applications that require it
--- }}}
-
--- Keyboard map indicator and switcher
+menubar.utils.terminal = terminal 
 mykeyboardlayout = awful.widget.keyboardlayout()
 
--- {{{ Wibar
--- Create a textclock widget
+-- Wibar
+---- Widget de reloj
 mytextclock = wibox.widget.textclock()
 
 -- Create a wibox for each screen and add it
@@ -133,7 +120,7 @@ local taglist_buttons = gears.table.join(
                                                   client.focus:toggle_tag(t)
                                               end
                                           end),
-                    awful.button({ }, 4, function(t) awful.tag.viewnext(t.screen) end),
+             	    awful.button({ }, 4, function(t) awful.tag.viewnext(t.screen) end),
                     awful.button({ }, 5, function(t) awful.tag.viewprev(t.screen) end)
                 )
 
@@ -209,16 +196,22 @@ awful.screen.connect_for_each_screen(function(s)
         { -- Left widgets
             layout = wibox.layout.fixed.horizontal,
             mylauncher,
+	    s.mylayoutbox,
             s.mytaglist,
             s.mypromptbox,
         },
         s.mytasklist, -- Middle widget
         { -- Right widgets
             layout = wibox.layout.fixed.horizontal,
-            mykeyboardlayout,
+--	    	ram,
+--		cpu,
+--		brillo,
+--		volumen,
+		bateria,
+--	    mykeyboardlayout,
             wibox.widget.systray(),
             mytextclock,
-            s.mylayoutbox,
+--	    s.mylayoutbox,
         },
     }
 end)
