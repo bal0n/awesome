5f95cfde6686ddcfbc09c4e26f29f24fdff04ec6
DEBIANSERVER <fjba@pm.me>
2019-12-03 18:14:56 +0100

    Depuracion de tm.lua

--- tm.lua
+++ tm.lua
@@ -17,61 +17,10 @@ local themes_path = gfs.get_themes_dir()
 
 local theme = {}
 theme.dir           = os.getenv("HOME") .. "/.config/awesome/themes/darkblue"
-theme.wallpaper     = theme.dir .. "/wallpapers/wall_0.png"
 theme.font          = "sans 8"
 theme.fontBat       = "Meslo LGS Regular 12"
---theme.wallpaper = "~/.config/awesome/src/aestetica3.jpg"
-
-
--- -- VENTANAS
--- theme.bg_normal     = "#151515" 
--- theme.bg_focus      = "#151515" 
--- theme.bg_urgent     = "#9e0000" 
--- theme.bg_minimize   = "#444444"
--- theme.bg_systray    = theme.bg_normal
-
--- -- COLOR DE FUENTES
--- theme.fg_normal     = "#ffffff"
--- theme.fg_focus      = "#ffffff"
--- theme.fg_urgent     = "#ffffff"
--- theme.fg_minimize   = "#ffffff"
-
--- -- BORDES
--- theme.useless_gap   = dpi(0)
--- theme.border_width  = dpi(1)
--- theme.border_normal = "#222222"
--- theme.border_focus  = "#222222"
--- theme.border_marked = "#222222"
-
--- -- TAGLIST
--- -- taglist_[bg|fg]_[focus|urgent|occupied|empty|volatile]
--- theme.taglist_bg_focus = "#555555";
--- theme.taglist_fg_focus = "#ffffff";
--- theme.taglist_bg_urgent = "#ff0000";
--- theme.taglist_fg_urgent = "#ffffff";
--- theme.taglist_bg_occupied = "#151515";
--- theme.taglist_fg_occupied = "#ffffff";
--- theme.taglist_bg_empty = "#151515";
--- theme.taglist_fg_empty = "#555555";
-
--- -- tasklist_[bg|fg]_[focus|urgent]
-
--- -- titlebar_[bg|fg]_[normal|focus]
--- theme.titlebar_bg_focus = "#151515";
--- theme.titlebar_fg_focus = "#ffffff";
-
--- -- tooltip_[font|opacity|fg_color|bg_color|border_width|border_color]
--- -- mouse_finder_[color|timeout|animate_timeout|radius|factor]
--- theme.mouse_finder_color = "#ff0000";
--- theme.mouse_finder_timeout = "#ff0000";
--- theme.mouse_finder_animate_timeout = "#ff0000";
--- theme.mouse_finder_radius = "#ff0000";
--- theme.mouse_finder_factor = "#ff0000";
-
--- prompt_[fg|bg|fg_cursor|bg_cursor|font]
--- hotkeys_[bg|fg|border_width|border_color|shape|opacity|modifiers_fg|label_bg|label_fg|group_margin|font|description_font]
--- Example:
---theme.taglist_bg_focus = "#ff0000"
+theme.wallpaper = "~/.config/awesome/src/aestetica3.jpg"
+--theme.wallpaper     = theme.dir .. "/wallpapers/wall_0.png"
 
 theme.bg_normal                                 = "#32302f"
 theme.fg_normal                                 = "#a89984"
@@ -106,6 +55,17 @@ theme.menu_submenu_icon                         = theme.dir .. "/icons/submenu.p
 theme.awesome_icon                              = theme.dir .. "/icons/awesome.png"
 theme.menu_height                               = dpi(15)
 theme.menu_width                                = dpi(100)
+theme.green                                     = "#00b159"
+theme.red                                       = "#d64d4d"
+theme.yellow                                    = "#cc9c00"
+theme.blue                                      = "#428bca"
+theme.darkred                                   = "#c92132"
+theme.darkgreen                                 = "#4d7358"
+theme.darkyellow                                = "#f18e38" 
+theme.gray                                      = "#5e5e5e"
+theme.violet                                    = "#8c8ccd"
+theme.pink                                      = "#B85C8A"
+theme.black                                     = theme.bg_normal
 
 -- Generate taglist squares:
 local taglist_square_size = dpi(4)
@@ -138,7 +98,6 @@ theme.widget_battery_medium                     = theme.dir .. "/icons/widgets/b
 theme.widget_battery_low                        = theme.dir .. "/icons/widgets/battery_low.png"
 theme.widget_battery_empty                      = theme.dir .. "/icons/widgets/battery_empty.png"
 theme.widget_battery_no                         = theme.dir .. "/icons/widgets/battery_no.png"
-
 theme.widget_mem                                = theme.dir .. "/icons/widgets/mem.png"
 theme.widget_cpu                                = theme.dir .. "/icons/widgets/cpu.png"
 theme.widget_temp                               = theme.dir .. "/icons/widgets/temp.png"
@@ -149,64 +108,25 @@ theme.widget_music                              = theme.dir .. "/icons/widgets/n
 theme.widget_music_on                           = theme.dir .. "/icons/widgets/note_on.png"
 theme.widget_music_pause                        = theme.dir .. "/icons/widgets/pause.png"
 theme.widget_music_stop                         = theme.dir .. "/icons/widgets/stop.png"
-
 theme.widget_vol                                = theme.dir .. "/icons/widgets/vol.png"
 theme.widget_vol_low                            = theme.dir .. "/icons/widgets/vol_low.png"
 theme.widget_vol_no                             = theme.dir .. "/icons/widgets/vol_no.png"
 theme.widget_vol_mute                           = theme.dir .. "/icons/widgets/vol_mute.png"
-
 theme.tasklist_plain_task_name                  = true
 theme.tasklist_disable_icon                     = true
 theme.useless_gap                               = 7
 
 theme.titlebar_close_button_focus               = theme.dir .. "/icons/titlebar/close_focus.png"
-
 theme.titlebar_ontop_button_focus_active        = theme.dir .. "/icons/titlebar/ontop_focus_active.png"
---theme.titlebar_ontop_button_normal_active       = theme.dir .. "/icons/titlebar/ontop_normal_active.png"
-
 theme.titlebar_ontop_button_focus_inactive      = theme.dir .. "/icons/titlebar/ontop_focus_inactive.png"
---theme.titlebar_ontop_button_normal_inactive     = theme.dir .. "/icons/titlebar/ontop_normal_inactive.png"
-
 theme.titlebar_sticky_button_focus_active       = theme.dir .. "/icons/titlebar/sticky_focus_active.png"
---theme.titlebar_sticky_button_normal_active      = theme.dir .. "/icons/titlebar/sticky_normal_active.png"
-
 theme.titlebar_sticky_button_focus_inactive     = theme.dir .. "/icons/titlebar/sticky_focus_inactive.png"
---theme.titlebar_sticky_button_normal_inactive    = theme.dir .. "/icons/titlebar/sticky_normal_inactive.png"
-
 theme.titlebar_floating_button_focus_active     = theme.dir .. "/icons/titlebar/floating_focus_active.png"
---theme.titlebar_floating_button_normal_active    = theme.dir .. "/icons/titlebar/floating_normal_active.png"
-
 theme.titlebar_floating_button_focus_inactive   = theme.dir .. "/icons/titlebar/floating_focus_inactive.png"
---theme.titlebar_floating_button_normal_inactive  = theme.dir .. "/icons/titlebar/floating_normal_inactive.png"
-
 theme.titlebar_maximized_button_focus_active    = theme.dir .. "/icons/titlebar/maximized_focus_active.png"
---theme.titlebar_maximized_button_normal_active   = theme.dir .. "/icons/titlebar/maximized_normal_active.png"
-
 theme.titlebar_maximized_button_focus_inactive  = theme.dir .. "/icons/titlebar/maximized_focus_inactive.png"
---theme.titlebar_maximized_button_normal_inactive = theme.dir .. "/icons/titlebar/maximized_normal_inactive.png"
 
-
--- Generate Awesome icon:
-theme.awesome_icon = theme_assets.awesome_icon(
-    theme.menu_height, theme.bg_focus, theme.fg_focus
-)
-
--- Define the icon theme for application icons. If not set then the icons
--- from /usr/share/icons and /usr/share/icons/hicolor will be used.
---theme.icon_theme = nil
-
--- colors
-theme.green          = "#00b159"
-theme.red            = "#d64d4d"
-theme.yellow         = "#cc9c00"
-theme.blue           = "#428bca"
-theme.darkred        = "#c92132"
-theme.darkgreen      = "#4d7358"
-theme.darkyellow     = "#f18e38" 
-theme.gray           = "#5e5e5e"
-theme.violet         = "#8c8ccd"
-theme.pink           = "#B85C8A"
-theme.black          = theme.bg_normal
+theme.awesome_icon = theme_assets.awesome_icon(theme.menu_height, theme.bg_focus, theme.fg_focus)
 
 theme.w1 = theme.dir .. "/icons/display/1.png"
 theme.w2 = theme.dir .. "/icons/display/2.png"
@@ -225,23 +145,5 @@ theme.w14 = theme.dir .. "/icons/display/14.png"
 theme.w15 = theme.dir .. "/icons/display/15.png"
 theme.w16 = theme.dir .. "/icons/display/16.png"
 theme.w17 = theme.dir .. "/icons/display/17.png"
-
-w1 = wibox.widget.imagebox(theme.w1)
-w2 = wibox.widget.imagebox(theme.w2)
-w3 = wibox.widget.imagebox(theme.w3)
-w4 = wibox.widget.imagebox(theme.w4)
-w5 = wibox.widget.imagebox(theme.w5)
-w6 = wibox.widget.imagebox(theme.w6)
-w7 = wibox.widget.imagebox(theme.w7)
-w8 = wibox.widget.imagebox(theme.w8)
-w9 = wibox.widget.imagebox(theme.w9)
-w10 = wibox.widget.imagebox(theme.w10)
-w11 = wibox.widget.imagebox(theme.w11)
-w12 = wibox.widget.imagebox(theme.w12)
-w13 = wibox.widget.imagebox(theme.w13)
-w14 = wibox.widget.imagebox(theme.w14)
-w15 = wibox.widget.imagebox(theme.w15)
-w16 = wibox.widget.imagebox(theme.w16)
-w17 = wibox.widget.imagebox(theme.w17)
 
 return theme
