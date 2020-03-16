ae785e9807b15daf538c93440b256611aee99fe2
terminator <fjba@pm.me>
2020-03-17 00:41:32 +0100

    Autorun y urxvtc como demonio

--- rc.lua
+++ rc.lua
@@ -43,20 +43,12 @@ do
 end
 -- }}}
 
--- {{{ Variable definitions
--- Themes define colours, icons, font and wallpapers.
+-- Variable definitions
+awful.spawn.with_shell("~/.config/awesome/scripts/autorun.sh") -- Autorun mediante script
 beautiful.init("~/.config/awesome/tema/tm.lua")
-
--- This is used later as the default terminal and editor to run.
-terminal = "urxvt"
+terminal = "urxvtc"
 editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
-
--- Default modkey.
--- Usually, Mod4 is the key with a logo between Control and Alt.
--- If you do not like this or do not have such a key,
--- I suggest you to remap Mod4 to another key using xmodmap or other tools.
--- However, you can use another modifier like Mod1, but it may interact with others.
 modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
