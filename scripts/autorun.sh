252649ce7c0af6410779247360604591489a92b9
fjba <fjba@pm.me>
2020-04-28 13:40:34 +0200

    Ajustes de scripts y añadido ntpd -qg

--- scripts/autorun.sh
+++ scripts/autorun.sh
@@ -8,10 +8,11 @@ function run {
 }
 
 # Programas a ejecutar.
-run compton
-run xbindkeys
-run unclutter
-run numlockx
-run mate-power-manager
-run urxvtd -q
-run xrdb -merge ~/.Xresources
+run compton                   # Compton para gestión de transparencias
+run xbindkeys                 # Atajos de teclado con xbindkeys (config en .xbindkeysrc)
+run unclutter                 # Unclutter para ocultar el ratón
+run numlockx                  # Marca numlock (Bloq Num)
+run mate-power-manager        # Carga el Power Manager de Mate
+run urxvtd -q                 # Ejecución del demonio de urxvt
+run xrdb -merge ~/.Xresources # Recarga de xrdb 
+run ntpd -qg                  # Ajuste de varios minutos en la hora con ntp
